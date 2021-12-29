38209198e1ffd193927b628fd2bcf404b74692ae
Özgün Aksoy <64470656+Ozgunak@users.noreply.github.com>
2021-12-29 16:19:43 +0300

    Chanced calculator app according to Standford CS193p lecture and chanced UI.

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -11,30 +11,86 @@ class CalculatorBrain {
     //istenen işlemler + - * / bonus C CE =
     //standford university ios 9 > youtube video 1 ya da 2
     private var accumulator: Double = 0
-    private var momento: Double = 0
-    private var firstOperant: Double = 0
-    private var lastOperator: String = ""
+//    private var momento: Double = 0
+//    private var firstOperant: Double = 0
+//    private var lastOperator: String = ""
     var result: Double {
         get {
             return accumulator
         }
     }
+    //MARK: - Complated after watching Stanford CS193P course, added C and CE over lecture
 
-    func performOperation(_ operation: String?) {
+    private var operations = [
+        "√" : Operation.UnaryOperator(sqrt),
+        "x" : Operation.BinaryOperator({$0 * $1}),
+        "÷" : Operation.BinaryOperator({$0 / $1}),
+        "+" : Operation.BinaryOperator({$0 + $1}),
+        "-" : Operation.BinaryOperator({$0 - $1}),
+        "=" : Operation.Equals,
+        "C" : Operation.Constant(0),
+        "CE" : Operation.CEReset
+    ]
+    
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperator((Double) -> Double)
+        case BinaryOperator((Double, Double) -> Double)
+        case Equals
+        case CEReset
+    }
+    private func executePending() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    func performOperation(_ operation: String) {
+        //MARK: - Complated after watching Stanford CS193P course
+        
+        if let calculate = operations[operation] {
+            switch calculate {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperator(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperator(let function):
+                executePending()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                executePending()
+            case .CEReset:
+                accumulator = 0
+                pending = nil
+            }
+        }
+
+        
+        
+        //MARK: - Complated homework after Bootcamp course
+        /*
         guard let operation = operation else { return }
-
+        
         switch operation {
         case "√":
             accumulator = sqrt(result)
         case "+":
             firstOperant = result
             lastOperator = "+"
-        case "/":
+        case "÷":
             firstOperant = result
-            lastOperator = "/"
-        case "*":
+            lastOperator = "÷"
+        case "x":
             firstOperant = result
-            lastOperator = "*"
+            lastOperator = "x"
         case "-":
             firstOperant = result
             lastOperator = "-"
@@ -58,8 +114,9 @@ class CalculatorBrain {
         default:
             break
         }
+         */
     }
-
+    
     func setOperand(_ value: Double) {
         accumulator = value
     }
