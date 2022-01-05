84c0f645e236a27e83375613618390971a55cd79
Özgün Aksoy <64470656+Ozgunak@users.noreply.github.com>
2022-01-06 00:02:17 +0300

    Added date formatter and chanced program root folder

--- Calculator/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -8,20 +8,20 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var resultLabel: UILabel!
-
+    
     @IBOutlet var digitButtons: [UIButton]!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     private var userIsTyping = false
-
+    
     private var brain = CalculatorBrain()
-
+    
     var result: Double {
         get {
             return Double(resultLabel.text ?? "0.0") ?? 0.0
@@ -30,18 +30,40 @@ class CalculatorViewController: UIViewController {
             resultLabel.text = String(newValue)
         }
     }
-
+    
     @IBAction func digitButtonTapped(_ sender: UIButton) {
-        if userIsTyping {
-            var resultText = String(Int(result))
-            resultText = resultText + String(sender.tag)
-            result = NSString(string: resultText).doubleValue
-        } else {
-            result = Double(sender.tag)
+        //MARK: - CS193p Codes
+        if sender.titleLabel?.text == "." {
+            let text = String(result) + "."
+            result = NSString(string: text).doubleValue
+        } else if userIsTyping {
+            let digit = String(sender.tag)
+            print(sender.tag)
+            var text = resultLabel.text!
+            text = text + digit
+            result = NSString(string: text).doubleValue
+            
+        } else{
+            let digit = String(sender.tag)
+            resultLabel.text = digit
             userIsTyping.toggle()
         }
+        
+        
+        //MARK: - Bootcamp codes
+        
+        /*
+         if userIsTyping {
+         var resultText = String(Int(result))
+         resultText = resultText + String(sender.tag)
+         result = NSString(string: resultText).doubleValue
+         } else {
+         resultLabel.text = String(sender.tag)
+         userIsTyping.toggle()
+         }
+         */
     }
-
+    
     @IBAction func operationButtonTapped(_ sender: UIButton) {
         userIsTyping = false
         brain.setOperand(result)
